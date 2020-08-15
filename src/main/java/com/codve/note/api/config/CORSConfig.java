9e3fdcfaed2d0ce5351a7a9e786ddc5b272529bc
jiangyu <jiangyuwise@gmail.com>
2020-08-16 00:45:10 +0800

    daily commit

---
+++ src/main/java/com/codve/note/api/config/CORSConfig.java
@@ -0,0 +1,31 @@
+package com.codve.note.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域访问配置
+ * @author admin
+ * @date 2020/8/16 00:28
+ */
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig());
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration corsConfig() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        return config;
+    }
+}
